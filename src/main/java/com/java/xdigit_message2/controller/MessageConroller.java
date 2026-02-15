f42d44d5b78af597e8d53b8a3d2d4d06b7b35ee0
majid-soliman <majidsoliman@gmail.com>
2026-02-15 18:28:09 +0400

    Adding Security origin

--- src/main/java/com/java/xdigit_message2/controller/MessageConroller.java
+++ src/main/java/com/java/xdigit_message2/controller/MessageConroller.java
@@ -7,6 +7,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +22,8 @@ import jakarta.servlet.http.HttpSession;
 
 
 
-@RestController 
+@RestController
+@CrossOrigin(origins="http://localhost:8082/")
 public class MessageConroller {
 
 	 @Autowired
